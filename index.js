fcfeb40afb661e15df4e9716995884e43dfad44b
keyur4103 <113822409+keyur4103@users.noreply.github.com>
2024-05-09 17:12:59 +0530

    first commit

--- index.js
+++ index.js
@@ -56,15 +56,18 @@ app.use("/posts", postRoutes);
 const PORT = process.env.PORT || 6001;
 
 mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    "mongodb+srv://kfataniya637:aDbnSIDTGEaijXEu@cluster1.3bs56qk.mongodb.net/socialpost",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
 
     /* ADD DATA ONE TIME */
-    User.insertMany(users);
-    Post.insertMany(posts);
+    // User.insertMany(users);
+    // Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
